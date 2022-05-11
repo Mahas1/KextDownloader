43c109e434997ab542902a3c37d83ae3f24ea7f3
Mahas1 <82939599+Mahas1@users.noreply.github.com>
2022-05-11 13:48:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,13 +10,21 @@ required_kexts = json.load(open("kexts.json"))
 kext_links = {i: kext_data[i]["versions"][0]["links"]["release"] for i in kext_data if i in required_kexts}
 
 try:
-    os.mkdir("kexts")
+    with open('config.json', 'r') as f:
+        config = json.load(f)
+except:
+    config = {}
+
+download_path = config.get("download_path", "kexts")
+
+try:
+    os.mkdir(download_path)
 except:
     pass
 
 for name, url in kext_links.items():
     kext_file = requests.get(url).content
-    with open(os.path.join("kexts", name + ".zip"), "wb") as f:
+    with open(os.path.join(download_path, name + ".zip"), "wb") as f:
         f.write(kext_file)
     print("Downloaded {}".format(name))
 
